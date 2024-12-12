219b984e7e672fde5c760a632f06916486df202c
Gabriel Xavier <k2.gabriel@tigre.com>
2024-12-12 11:29:52 -0300

    Pagina de contato

--- app.vue
+++ app.vue
@@ -4,6 +4,7 @@
       <ul>
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/contact">Contact</NuxtLink></li>
       </ul>
     </nav>
     <NuxtPage />
